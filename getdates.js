6316ec597ae2b33623d833849c382e14d3a958f8
steph-1479 <antwiwaahstephanie71@gmail.com>
2025-07-11 03:42:39 -1200

    base.css

--- getdates.js
+++ getdates.js
@@ -1,20 +1,19 @@
 
 
-    // Get the current date
+    
     const now = new Date();
     const currentYear = now.getFullYear();
 
-    // Format the date: e.g., July 9, 2025
+  
     const options = { year: '2025', month: 'July', day: '8' };
     const formattedDate = now.toLocaleDateString(undefined, options);
 
-    // Insert into copyright paragraph
+
     const copyrightEl = document.getElementById("copyright");
     if (copyrightEl) {
       copyrightEl.innerHTML = `&copy; ${currentYear} Obeng Antwiwaah Stephanie. All rights reserved. Date: ${formattedDate}`;
     }
 
-    // Insert document last modified date
     const lastModified = document.lastModified;
     const dateEl = document.getElementById("date");
     if (dateEl) {
