341c0fdcc5729031bc1e04a51a4527942b678259
steph-1479 <antwiwaahstephanie71@gmail.com>
2026-02-07 14:08:14 -1200

    FORM.HTML

--- form.js
+++ form.js
@@ -13,3 +13,23 @@ products.forEach(product => {
     option.textContent = product.name;
     productSelect.appendChild(option);
 });
+
+const now = new Date();
+    const currentYear = now.getFullYear();
+
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = now.toLocaleDateString(undefined, options);
+
+
+    const copyrightEl = document.getElementById("copyright");
+    if (copyrightEl) {
+      copyrightEl.innerHTML = `&copy; ${currentYear} Obeng Antwiwaah Stephanie. All rights reserved. Date: ${formattedDate}`;
+    }
+
+    
+    const lastModified = document.lastModified;
+    const dateEl = document.getElementById("date");
+    if (dateEl) {
+      dateEl.textContent = `Last Modified: ${lastModified}`;}
+
+     
